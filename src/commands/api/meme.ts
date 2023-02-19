7821dcc8dbc023fa7b5f950072cfde16e32768d6
Grayson-code <northernstarmusic10@gmail.com>
2023-02-19 17:50:24 +0530

    feat: :sparkles: new reaction roles and fix meme

--- src/commands/api/meme.ts
+++ src/commands/api/meme.ts
@@ -1,13 +1,15 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Command } from '@sapphire/framework';
 import { EmbedBuilder, type Message } from 'discord.js';
+import { fetch, FetchResultTypes } from '@sapphire/fetch';
+
 import type { API } from '../../../types/meme'
 @ApplyOptions<Command.Options>({
 	description: 'A basic command'
 })
 export class UserCommand extends Command {
 	public async messageRun(message: Message) {
-		const meme = await fetch("https://meme-api.com/gimme").catch(e => this.container.logger.error(e)) as unknown as API
+		const meme = await fetch<API>("https://meme-api.com/gimme", FetchResultTypes.JSON)
 		const embed = new EmbedBuilder()
 		.setTitle(meme.title)
 		.setURL(meme.postLink)
--- src/listeners/messageReactionAdd.ts
+++ src/listeners/messageReactionAdd.ts
@@ -8,7 +8,7 @@ export class UserEvent extends Listener {
 		if (messageReaction.partial) {
 			await messageReaction.fetch().catch();
 		}
-		if (messageReaction.message.id !== '1076060764860854272') {
+		if (messageReaction.message.channel.id !== '1076059315644928052') {
 			return;
 		}
 		switch (messageReaction.emoji.name) {
@@ -24,6 +24,36 @@ export class UserEvent extends Listener {
 			case '🖥️':
 				this.addReaction('1076058696058150942', messageReaction, user);
 				break;
+			case '♂️':
+				this.addReaction('1076769787444731955', messageReaction, user);
+				break;
+			case '♀️':
+				this.addReaction('1076769834802630726', messageReaction, user);
+				break;
+			case '😁':
+				this.addReaction('1076769868101202050', messageReaction, user);
+				break;
+			case '🇻':
+				this.addReaction('1076769978948272138', messageReaction, user);
+				break;
+			case '🇲':
+				this.addReaction('1076770020652232757', messageReaction, user);
+				break;
+			case '🇨':
+				this.addReaction('1076770048816988222', messageReaction, user);
+				break;
+			case '🇫':
+				this.addReaction('1076770092223828028', messageReaction, user);
+				break;
+			case '🇦':
+				this.addReaction('1076770126122188840', messageReaction, user);
+				break;
+			case '🔫':
+				this.addReaction('1076770185932972105', messageReaction, user)
+				break
+			case '🇱':
+				this.addReaction('1076770257873674260', messageReaction, user)
+				break
 			default:
 				console.log(messageReaction.emoji.name);
 				console.log('your emojis got some problem');
