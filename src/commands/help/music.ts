d070efb1e2c2fd31ae6d34f35301ab29e8c88f0a
Grayson-code <northernstarmusic10@gmail.com>
2023-03-04 16:29:22 +0530

    typo

--- src/commands/help/music.ts
+++ src/commands/help/music.ts
@@ -12,13 +12,14 @@ export class UserCommand extends Command {
 			.setFields([
 				{ name: '!play [music you want to play]', value: 'Starts playing a song, playlist, and if already playing adds the song or playlist to the queue. Eg: `!play guantanamo` or `!p https://open.spotify.com/playlist/xyz`' },
 				{ name: '!skip', value: 'Skips the current song that is playing. Eg: `!sk`' },
-				{ name: '!queue [optiona, the page number]', value: 'Shows the current queue. Eg: `!queue` or `!q 1`' },
+				{ name: '!queue [optional, the page number]', value: 'Shows the current queue. Eg: `!queue` or `!q 1`' },
 				{ name: '!pause', value: 'Pauses the current playing song. Eg:`!pa`' },
 				{ name: '!resume', value: 'Resume the paused player. Eg:`!res' },
 				{ name: '!stop', value: 'Destroys the queue entirely. Eg:`!stop`' },
 				{name: "!shuffle", value: 'Shuffles the queue. Eg: `!sh`'},
 				{name: "!nowplaying", value: "Shows the song that is currently playing. Eg: `!np`"}
-			]);
+			])
+			.setColor("Blurple");
 		return message.channel.send({ embeds: [embed] });
 	}
 }
